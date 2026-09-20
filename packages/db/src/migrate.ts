d31027ece3e920a8238f088903b5737149d177cd
hxy91819 <masonxhuang@proton.me>
2026-09-20 15:56:50 +0800

    fix: target rebased Recent migration
    (cherry picked from commit 16ffdfe9f4e1af93efed7fc8c39887106970e3ae)
    (cherry picked from commit b0ce1b3b4628ae0a56a9614e307f03479afd1a8c)

--- packages/db/src/migrate.ts
+++ packages/db/src/migrate.ts
@@ -1280,7 +1280,7 @@ function stageExistingRecentWorkSequenceColumn(
 
   const migration = requireExpectedAppliedMigration(
     readExpectedAppliedMigrations(migrationsFolder),
-    "0119_magenta_dexter_bennett",
+    "0120_stale_chamber",
   );
   if (readAppliedMigrationCreatedAts(db).has(migration.createdAt)) return;
 
