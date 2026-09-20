e2e0533cac26ad85706b56d5eee7c837d2bbc159
hxy91819 <masonxhuang@proton.me>
2026-09-20 15:55:10 +0800

    fix: default pinnedThreads when rendering ProjectModeSections
    (cherry picked from commit dab233b5de6d342892fd1e0930badd420a7e321f)
    (cherry picked from commit e2f17687e16f134112900820c5accdbaf371ae42)

--- apps/app/src/components/sidebar/ProjectList.tsx
+++ apps/app/src/components/sidebar/ProjectList.tsx
@@ -678,7 +678,7 @@ export function ProjectModeSections({
   pinnedReorderPending,
   pinnedRootNodes,
   pinnedSection,
-  pinnedThreads,
+  pinnedThreads = [],
   onReorderPinnedThread,
   projects,
   selectedThreadId,
